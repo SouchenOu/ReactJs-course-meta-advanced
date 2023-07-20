e6b2a0e7122901f66d4482e453436e392ac8cd88
Soukaina Ouchen <souchen@e3r8p1.1337.ma>
2023-07-20 09:40:35 +0100

    implement challenge 2

--- course.js
+++ course.js
@@ -7,7 +7,7 @@ ReactDOM.render(<h1>Hello react!</h1>, document.getElementById("id1")); // React
 ReactDOM.render(<p>Hello from paragraph</p>, document.getElementById("id1"));
 ReactDOM.render(<ul><li>Home</li><li>page1</li></ul>, document.getElementById("id2"));
 
-/***********Challenge1: */
+/***********Challenge1: ****************************/
 
 /**Create your own custom React component!
  * call it "MainContent", and have it return a simple
@@ -23,37 +23,37 @@ function Navbar()
 {
     return(
 
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
@@ -68,3 +68,26 @@ function MainContent()
 }
 
 ReactDOM.render(<div><Navbar/><MainContent/></div>, document.getElementById("navbar"));
+
+/***********challenge 2************** */
+
+/*****challenge - recreate the above line of code in vanilla JS by creating and appending an h1 to our div#root (without using innerHTML) */
+
+/*****
+ * - Create a new h1 element
+ * Give it some textContent
+ * Give it a class name of "header"
+ * append it as a child of div#root
+ * 
+ * 
+ */
+
+// Method1
+const h1 = document.createElement("h1");
+h1.textContent = "Hello souchen lets learn react";
+h1.className = "header";
+document.getElementById("root").append(h1);
+//Method2
+
+ReactDOM.render(<h1 className="header">Hello souchen let's learn ReactJs</h1>, document.getElementById("root"));
+
