466551db6622970b3b56a989833fd9dfd8f97985
Soukaina Ouchen <souchen@e3r8p1.1337.ma>
2023-07-21 07:38:21 +0100

    rendring the contenue of a variable

--- my-app/src/challenge3.js
+++ my-app/src/challenge3.js
@@ -3,7 +3,9 @@
 
 /*******
  * 
- * Add a 'header' element with a nested 'vav' element. Inside the 'nav', include a 'img' element with the image of the React logo inside (src = "./react-logo.png" and make sure to set the width to something more manageable so it doesn't take up the whole screen )
+ * Add a 'header' element with a nested 'nav' element. Inside the 'nav', include a 'img' element with the image of the React logo inside (src = "./react-logo.png" and make sure to set the width to something more manageable so it doesn't take up the whole screen )
  * 
  * 
  */
+
+
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,14 +7,46 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
+//This is a variable
+const page = (
+<div>
+  <img src="./images/react-logo.png" alt="" width="40px"/>
+  <h1>Fun facts about React</h1>
+  <ul>
+      <li>Was first </li>
+      <li>Was second </li>
+      <li>Was third </li>
+  </ul>
+</div>
+
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//Method1
+root.render(page); 
+
+//Method2
+root.render(
+  <div>
+    <img src="./images/react-logo.png" alt="" width="40px"/>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first </li>
+      <li>Was second </li>
+      <li>Was third </li>
+      </ul>
+  </div>)
+
+ /*********************** */
 root.render(
   <React.StrictMode>
     <App />
     <Challenge2 />
   </React.StrictMode>
 );
+
+/************************ */
 
 
 reportWebVitals();
